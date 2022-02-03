8f8c38f84dd87b76aea091938daf61070e4510f4
Olli Raitio <olli.raitio@helsinki.fi>
2022-02-03 19:32:39 +0200

    customized publish script

--- build/publish.sh
+++ build/publish.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-npm update
-
-VERSION=$(node --eval "console.log(require('./package.json').version);")
-
 npm test || exit 1
 
-git checkout -b build
-
 jake build[,,true]
-jake docs
 
 git add \
     dist/leaflet.draw.js \
@@ -20,12 +13,4 @@ git add \
     docs/* \
     -f
 
-git commit -m "v$VERSION"
-
-git tag v$VERSION -f
-git push --tags -f
-
-npm publish
-
-git checkout master
-git branch -D build
+git commit -m "bump"
